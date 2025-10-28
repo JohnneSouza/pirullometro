034057982f212bc56b6665c1a5245f98556f1a0e
Johnnes Souza <jhonnesf5@gmail.com>
2025-10-27 23:07:55 -0300

    Fix missing space on lines

--- backend/src/main/java/dev/kangoo/pirullometro/rest/YouTubeWebClient.java
+++ backend/src/main/java/dev/kangoo/pirullometro/rest/YouTubeWebClient.java
@@ -2,6 +2,7 @@ package dev.kangoo.pirullometro.rest;
 
 
 import dev.kangoo.pirullometro.domain.response.youtube.YoutubeVideoResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,19 +10,18 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Component
 public class YouTubeRestClient {
 
+    private final String youtubeApiKey;
     private final WebClient youTubeWebClient;
 
-    public YouTubeRestClient(WebClient youTubeWebClient) {
+    public YouTubeRestClient(@Value("${youtube.api.key}") String apiKey,
+                             WebClient youTubeWebClient) {
         this.youTubeWebClient = youTubeWebClient;
+        this.youtubeApiKey = apiKey;
     }
 
     public YoutubeVideoResponse getVideoDetails(String part, String videoId) {
         return this.youTubeWebClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/youtube/v3/videos")
-                        .queryParam("part", part)
-                        .queryParam("id", videoId)
-                        .build())
+                .uri("/youtube/v3/videos?part={part}&id={id}&key={key}", part, videoId, this.youtubeApiKey)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(YoutubeVideoResponse.class)
